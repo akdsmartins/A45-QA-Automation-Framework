bb38456c26c306fdceef9a3e5f05c4026df5748f
akdsmartins <131289483+akdsmartins@users.noreply.github.com>
2023-04-24 16:01:32 -0700

    addSongToPlaylist()

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -13,42 +12,41 @@ import java.time.Duration;
 public class BaseTest {
     WebDriver driver;
 
-    @BeforeMethod
-    public void setUpBrowser() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+    @BeforeSuite
+    static void setupClass () {
+        WebDriverManager.chromedriver().setup();
+    }
 
+    @BeforeMethod
+    public void setupBrowser() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
-    @BeforeMethod
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+
     public void openLoginUrl () {
         driver.get("https://bbb.testpro.io/");
     }
 
-    @BeforeMethod
     public void enterValidEmail (String email) {
         WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-        emailField.click();
+        emailField.clear();
         emailField.sendKeys(email);
     }
 
-    @BeforeMethod
     public void enterValidPassword (String password) {
         WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
-        passwordField.click();
+        passwordField.clear();
         passwordField.sendKeys(password);
     }
-
-    @BeforeMethod
-    public void clickSubmit () {
+    public void clickSubmit () throws InterruptedException {
         WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
         submitButton.click();
-    }
-    @AfterMethod
-    public void closeBrowser() {
-        driver.quit();
+        Thread.sleep(2000);
     }
 
 }
--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -5,34 +5,59 @@ import org.testng.annotations.Test;
 
 public class Homework17 extends BaseTest {
     @Test
-    public void addSongToPlaylist() {
+    public void addSongToPlaylist() throws InterruptedException {
 
-        //preconditions
-        setUpBrowser();
+        //preconditions set in BaseTest
         openLoginUrl();
         enterValidEmail("amanda.martins@testpri.io");
         enterValidPassword("te$t$tudent");
         clickSubmit();
 
-        //add song to playlist test
+        //test case call methods
+        searchSong("dark days");
+        clickViewAll();
+        selectFirstSong();
+        clickAddTo();
+        selectPlaylist();
+        verifyMessage();
+
+    }
+
+    public void searchSong(String title) throws InterruptedException {
         WebElement searchField = driver.findElement(By.cssSelector("[type='search']"));
         searchField.click();
-        searchField.sendKeys("dark days");
+        searchField.sendKeys(title);
+        Thread.sleep(2000);
+    }
 
+    public void clickViewAll() throws InterruptedException {
         WebElement viewAllButton = driver.findElement(By.cssSelector("[data-test='view-all-songs-btn']"));
         viewAllButton.click();
+        Thread.sleep(2000);
+    }
 
+    public void selectFirstSong() throws InterruptedException {
         WebElement firstSong = driver.findElement(By.cssSelector("tr.song-item.playing.selected td:nth-child(2)"));
         firstSong.click();
+        Thread.sleep(2000);
+    }
 
+    public void clickAddTo() throws InterruptedException {
         WebElement addTo = driver.findElement(By.cssSelector("[title='Add selected songs to…']"));
         addTo.click();
+        Thread.sleep(2000);
+    }
 
-        WebElement addToFavorites = driver.findElement(By.cssSelector("#songResultsWrapper li:nth-child(5)"));
-        addToFavorites.click();
+    public void selectPlaylist() throws InterruptedException {
+        WebElement addToPlaylist = driver.findElement(By.cssSelector("#songResultsWrapper li:nth-child(5)"));
+        addToPlaylist.click();
+        Thread.sleep(2000);
+    }
 
+    public void verifyMessage() throws InterruptedException {
         WebElement notificationMessage = driver.findElement(By.cssSelector("div.success.show"));
-        Assert.assertEquals(notificationMessage.isDisplayed(), true);
+        Assert.assertTrue(notificationMessage.isDisplayed());
+        Thread.sleep(2000);
     }
 
 
