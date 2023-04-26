bf907764556f26df8bc53c3717f4580b3e18f8cb
akdsmartins <131289483+akdsmartins@users.noreply.github.com>
2023-04-26 11:54:42 -0700

    Homework18 playSong()

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,28 +1,30 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework18 extends BaseTest{
 
   @Test
-  public void playSong(){
+  public void playSong() throws InterruptedException {
    navigateToPage();
    provideEmail("amanda.martins@testpro.io");
    providePassword("te$t$tudent");
    clickSubmit();
+   Thread.sleep(2000);
 
    WebElement clickPlayNextSong = driver.findElement(By.cssSelector(".next.fa.fa"));
    clickPlayNextSong.click();
+   Thread.sleep(2000);
 
    WebElement clickPlaySong = driver.findElement(By.cssSelector("[title='Play or resume']"));
    clickPlaySong.click();
+   Thread.sleep(2000);
 
    WebElement verifySongPlaying = driver.findElement(By.cssSelector("span.pause"));
+   Assert.assertTrue(verifySongPlaying.isDisplayed());
+   Thread.sleep(2000);
 
-
-
-
-     
  }
 
 
