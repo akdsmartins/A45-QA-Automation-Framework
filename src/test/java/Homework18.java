dd523a440eb1a5696fced6e9363536385a4cdc20
akdsmartins <131289483+akdsmartins@users.noreply.github.com>
2023-04-26 11:32:29 -0700

    playSong

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,9 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class Homework18 {
+public class Homework18 extends BaseTest{
 
- @Test
- public void playSong(){
+  @Test
+  public void playSong(){
+   navigateToPage();
+   provideEmail("amanda.martins@testpro.io");
+   providePassword("te$t$tudent");
+   clickSubmit();
+
+   WebElement clickPlayNextSong = driver.findElement(By.cssSelector(".next.fa.fa"));
+   clickPlayNextSong.click();
+
+   WebElement clickPlaySong = driver.findElement(By.cssSelector("[title='Play or resume']"));
+   clickPlaySong.click();
+
+   WebElement verifySongPlaying = driver.findElement(By.cssSelector("span.pause"));
+
+
 
 
      
