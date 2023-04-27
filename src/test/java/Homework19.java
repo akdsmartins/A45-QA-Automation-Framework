a778d1812c4819be736b58d5a72f450b5f38d56b
akdsmartins <131289483+akdsmartins@users.noreply.github.com>
2023-04-27 10:54:19 -0700

    Homework19

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class Homework19 extends BaseTest {
+    @Test
+    public void deletePlaylist(){
+        launchBrowser(url);
+        provideEmail("amanda.martins@testpro.io");
+        providePassword("te$t$tudent");
+        
+
+
+    }
+}
