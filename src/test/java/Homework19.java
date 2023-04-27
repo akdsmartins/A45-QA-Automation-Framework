fc958c1833862879cff02c1572dfa38ebb538d10
akdsmartins <131289483+akdsmartins@users.noreply.github.com>
2023-04-27 11:09:28 -0700

    Homework19

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class Homework19 extends BaseTest {
@@ -6,7 +8,16 @@ public class Homework19 extends BaseTest {
         launchBrowser(url);
         provideEmail("amanda.martins@testpro.io");
         providePassword("te$t$tudent");
-        
+
+        WebElement selectPlaylist = driver.findElement(By.cssSelector(".side.side-nav #playlists li:nth-child(3)"));
+        selectPlaylist.click();
+
+        WebElement deletePlaylist = driver.findElement(By.cssSelector(".del.btn-delete-playlist"));
+        deletePlaylist.click();
+
+        WebElement notificationMessage = driver.findElement(By.cssSelector(".success.show"));
+
+
 
 
     }
