cdeb7b4cecf70cae5aea2cc63b3f4eda1a496cc0
akdsmartins <131289483+akdsmartins@users.noreply.github.com>
2023-05-02 11:22:50 -0700

    Homework20

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,20 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.time.Duration;
-import java.util.List;
 import java.util.UUID;
 
 public class BaseTest {
+
     public static WebDriver driver = null;
-    public static WebDriverWait wait = null;
-    public static Actions actions = null;
+
     public static String url = "";
 
     @BeforeSuite
@@ -43,52 +38,48 @@ public class BaseTest {
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        actions = new Actions(driver);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         url = BaseURL;
-        navigateToPage();
+        driver.get(url);
     }
-
-    @AfterMethod(enabled = false)
+    @AfterMethod
     public void closeBrowser() {
         driver.quit();
     }
 
-    public static void navigateToPage() {
-        driver.get(url);
-    }
 
     public static void provideEmail(String email) {
-        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.click();//not needed
         emailField.clear();
         emailField.sendKeys(email);
     }
 
     public static void providePassword(String password) {
-        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.click();//not needed
         passwordField.clear();
         passwordField.sendKeys(password);
     }
 
     public static void clickSubmit() {
-        WebElement submit = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
         submit.click();
     }
 
     public static void clickSaveButton() {
-        WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.btn-submit")));
+        WebElement saveButton = driver.findElement(By.cssSelector("button.btn-submit"));
         saveButton.click();
     }
 
     public static void provideProfileName(String randomName) {
-        WebElement profileName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
+        WebElement profileName = driver.findElement(By.cssSelector("[name='name']"));
         profileName.clear();
         profileName.sendKeys(randomName);
     }
 
     public static void provideCurrentPassword(String password) {
-        WebElement currentPassword = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='current_password']")));
+        WebElement currentPassword = driver.findElement(By.cssSelector("[name='current_password']"));
         currentPassword.clear();
         currentPassword.sendKeys(password);
     }
@@ -98,43 +89,7 @@ public class BaseTest {
     }
 
     public static void clickAvatarIcon() {
-        WebElement avatarIcon = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("img.avatar")));
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
         avatarIcon.click();
-    }
-
-    // hover
-    public void hoverPlay() {
-        WebElement play = driver.findElement(By.cssSelector("[data-testid='play-btn']"));
-        // move to element
-        actions.moveToElement(play).perform();
-    }
-
-    // context click
-    public void chooseAllSongsList() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li a.songs")));
-        driver.findElement(By.cssSelector("li a.songs")).click();
-    }
-
-    public void contextClickFirstSong(){
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".all-songs tr.song-item:nth-child(1)")));
-        WebElement firstSong = driver.findElement(By.cssSelector(".all-songs tr.song-item:nth-child(1)"));
-        // context click
-        actions.contextClick(firstSong).perform();
-    }
-
-    public void displayAllSongs() {
-        chooseAllSongsList();
-    //add assertion
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".all-songs tr.song-item")));
-        List<WebElement> songsList = driver.findElements(By.cssSelector(".all-songs tr.song-item"));
-        Assert.assertEquals(songsList.size(), 63);
-    }
-
-    // double click
-    public void doubleClickChoosePlaylist() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
-        // double click
-        WebElement playlist = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
-        actions.doubleClick(playlist).perform();
     }
 }
