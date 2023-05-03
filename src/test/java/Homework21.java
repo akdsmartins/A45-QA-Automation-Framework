b4d3dd7d4bb0a8d38ad25ef34c2535731f090756
akdsmartins <131289483+akdsmartins@users.noreply.github.com>
2023-05-03 14:18:39 -0700

    Homework21

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -1,27 +1,41 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 public class Homework21 extends BaseTest{
     @Test
     public void renamePlaylist(){
+
+        String newPlaylistName = "MyPlaylist";
+
         navigateToPage();
-        provideEmail("amanda.martins@tetpro.io");
+        provideEmail("amanda.martins@testpro.io");
         providePassword("te$t$tudent");
         clickSubmit();
+        rightClickOnPlaylist();
+        selectEdit();
+        changeName();
+    }
 
+    public void rightClickOnPlaylist () {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#mainWrapper #sidebar #playlists li:nth-child(3)")));
         actions.contextClick(driver.findElement(By.cssSelector("#mainWrapper #sidebar #playlists li:nth-child(3)"))).perform();
+    }
 
+    public void selectEdit () {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".menu.playlist-item-menu li:nth-child(1)")));
         actions.click(driver.findElement(By.cssSelector(".menu.playlist-item-menu li:nth-child(1)"))).perform();
+    }
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#mainWrapper #sidebar .active")));
-        actions.contextClick(driver.findElement(By.cssSelector("#mainWrapper #sidebar #playlists li:nth-child(3)"))).perform();
+    public void changeName () {
+        WebElement editField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[data-testid='inline-playlist-name-input']")));
+        editField.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.BACK_SPACE));
+        editField.sendKeys("MyPlaylist");
+        editField.sendKeys(Keys.ENTER);
 
-
-
-
-
+        WebElement upDatedName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#mainWrapper #sidebar #playlists li:nth-child(3)")));
 
     }
 }
