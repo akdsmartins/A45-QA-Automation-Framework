9f92f843a4e118ec37aeb7457be29d4ef8a8a907
akdsmartins <131289483+akdsmartins@users.noreply.github.com>
2023-05-26 15:48:53 -0700

    1

--- src/test/java/BasePage.java
+++ src/test/java/BasePage.java
@@ -1,8 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeSuite;
 
-public class BaseTest {
+public class BasePage {
 
     @BeforeSuite
     static void setupClass() {
