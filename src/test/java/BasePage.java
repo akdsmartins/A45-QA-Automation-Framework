d81015ff85401c8534208c1b117d8af6261f1e47
akdsmartins <131289483+akdsmartins@users.noreply.github.com>
2023-05-26 16:03:32 -0700

    Intern

--- src/test/java/BasePage.java
+++ src/test/java/BasePage.java
@@ -1,10 +1,35 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import javax.swing.*;
 
 public class BasePage {
 
+    //Important variable to be declared first
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+    public static Action actions;
+    public static String url = "https://bbb.testpro.io/";
+
+
+    //Always set up the browser in before suite
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+        //WebDriverManager.edgedriver().setup();
     }
+
+    //Launch the browser in before method
+    @BeforeMethod
+    static void launchBrowser (){
+        //Added ChromeOptions argument below to fix websocket errorAdded ChromeOptions argument below to fix websocket error
+
+
+    }
+
+
+
 }
