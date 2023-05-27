981cead73d9fc6bace7200f8a724f0590f2bc4a6
akdsmartins <131289483+akdsmartins@users.noreply.github.com>
2023-05-26 17:47:31 -0700

    intern

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,12 @@
+import org.testng.annotations.Test;
+
 public class LoginTests extends BasePage{
 
-    
+    @Test
+    public void LoginValidEmailAndPassword (){
+        LoginPage loginPage = new LoginPage(driver);
+
+    }
+
+
 }
